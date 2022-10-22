e82a1366fd620e0b592ec3e6dc51eca550bf90b1
Elisha Lai <elisha.lai@icloud.com>
2022-10-22 18:30:33 -0400

    feat: add codepipeline to pipeline stack

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,14 +1,31 @@
 import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
+
 import { Construct } from "constructs";
 
 class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new codecommit.Repository(this, "WorkshopRepo", {
+    const repository = new codecommit.Repository(this, "WorkshopRepo", {
       repositoryName: "CDKWorkshop",
+    });
+
+    const synth = new CodeBuildStep("SynthStep", {
+      input: CodePipelineSource.codeCommit(repository, "main"),
+      installCommands: ["npm install -g aws-cdk"],
+      commands: ["npm ci", "npm run build", "npx cdk synth"],
+    });
+
+    const pipeline = new CodePipeline(this, "CodePipeline", {
+      pipelineName: "WorkshopPipeline",
+      synth,
     });
   }
 }
