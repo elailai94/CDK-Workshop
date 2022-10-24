e8c452178a482deccdfebc1c0c132497678d3e02
Elisha Lai <elisha.lai@icloud.com>
2022-10-24 16:33:26 -0400

    chore: update dependencies

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -5,4 +5,9 @@ import * as cdk from "aws-cdk-lib";
 import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new PipelineStack(app, "PipelineStack");
+new PipelineStack(app, "PipelineStack", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    }
+});
